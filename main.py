24251d7371ffe24ac8537a09b999d3569c2d7167
bucketpigeon <mt.skybox@gmail.com>
2024-12-14 21:06:01 -0800

    Created list of dictionary, sorted, printed to example

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ def main():
     text = book_text(book)
     num_words = word_count(text)
     num_chara = character_count(text)
+    list_of_dicts = iterate_over_dict(num_chara)
+    list_of_dicts.sort(reverse=True, key=sort_on)
     
     print(f"--- Begin report of {book} ---")
     print(f"{num_words} words found in the document")
-    for letter in num_chara:
-        num = num_chara[letter]
-        print(f"The '{letter}' character was found {num} times")
+    for dictionary in list_of_dicts:
+        print(f"The '{dictionary["letter"]}' character was found {dictionary["num"]} times")
     print("--- End report ---")
 
 def book_text(path):
@@ -29,6 +30,12 @@ def character_count(text):
             if i.isalpha() == True:
                 char_dict[i] = 1
     return char_dict
+
+def iterate_over_dict(dict):
+    variable = []
+    for i, j in dict.items():
+        variable.append({"letter": i, "num": j})
+    return variable
 
 def sort_on(dict):
     return dict["num"]
